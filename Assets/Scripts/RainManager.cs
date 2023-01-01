52bc556e66cf36efed45c54013729181413ff824
Vidya <bhushanjss@gmail.com>
2023-01-01 23:29:57 +0000

    simulation

---
+++ Assets/Scripts/RainManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject rainPrefab;
+    [SerializeField]
+    private GameObject rainFolder;
+
+    [SerializeField]
+    private float rangeXZ = 10f;
+    [SerializeField]
+    private float rangeMinY = 10f;
+    [SerializeField]
+    private float rangeMaxY = 50f;
+    [SerializeField]
+    private float rainSpeed = 1f;
+    [SerializeField]
+    private int _rainCount = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnRoutine());
+    }
+
+    IEnumerator SpawnRoutine()
+    {
+        while (true)
+        {
+            for(int i=0;i < _rainCount; i++)
+            {
+                float randomPosX = Random.Range(-rangeXZ, rangeXZ);
+                float randomPosZ = Random.Range(-rangeXZ, rangeXZ);
+                float randomPosY = Random.Range(rangeMinY, rangeMaxY);
+                Vector3 pos = new Vector3(randomPosX, randomPosY, randomPosZ);
+                GameObject rainDrop = Instantiate(rainPrefab);
+                rainDrop.transform.SetParent(rainFolder.transform);
+                rainDrop.transform.Translate(pos);
+            }            
+            float rSpeed = rainSpeed * 0.01f;
+            yield return new WaitForSeconds(rSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/drop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class drop : MonoBehaviour
+{
+
+    private float _speed = 9.8f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(Vector3.down * _speed * Time.deltaTime);
+
+        if(transform.position.y < 0f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
